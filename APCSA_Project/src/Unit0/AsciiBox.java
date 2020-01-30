e69587bd9fd91e2689aaf1e99cd606c1af161266
mike <mike@DESKTOP-H98JHIB.san.rr.com>
2020-01-29 20:48:29 -0800

    Upload Unit0

---
+++ APCSA_Project/src/Unit0/AsciiBox.java
@@ -0,0 +1,18 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name \t  date \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+
+	}
+}
