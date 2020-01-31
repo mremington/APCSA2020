2a3eb357f84275412d0643e1f4bdcec680d071fa
mremington <michael.remington@sduhsd.net>
2020-01-31 10:25:44 -0800

    upload unit0 from school

---
+++ APCS-A-2020/src/Unit0/AsciiBox.java
@@ -0,0 +1,40 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Mr Remington \t  1/30/20 \n\n" );
+		
+		for(int i =0; i<3;i++) {
+			System.out.println("+++++++++++++++++++++++++ " );
+			System.out.println("+++++++++++++++++++++++++ " );
+			System.out.println("+++++++++++++++++++++++++ " );
+			System.out.println("AAAAAAAAAAAAAAAAAAAAA ");
+			System.out.println("AAAAAAAAAAAAAAAAAAAAA ");
+			
+		}
+		
+		
+//		System.out.println("+++++++++++++++++++++++++ " );
+//		System.out.println("+++++++++++++++++++++++++ " );
+//		System.out.println("+++++++++++++++++++++++++ " );
+//		System.out.println("AAAAAAAAAAAAAAAAAAAAA ");
+//		System.out.println("AAAAAAAAAAAAAAAAAAAAA ");
+//		System.out.println("+++++++++++++++++++++++++ " );
+//		System.out.println("+++++++++++++++++++++++++ " );
+//		System.out.println("+++++++++++++++++++++++++ " );
+//		System.out.println("AAAAAAAAAAAAAAAAAAAAA ");
+//		System.out.println("AAAAAAAAAAAAAAAAAAAAA ");
+		
+		
+
+	}
+}
