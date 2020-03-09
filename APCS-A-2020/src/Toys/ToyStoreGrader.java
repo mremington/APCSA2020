d3a95acbae44dafce674be9f0c789f294b82a0ed
mremington <michael.remington@sduhsd.net>
2020-03-09 12:00:10 -0700

    data project

--- APCS-A-2020/src/Toys/ToyStoreGrader.java
+++ APCS-A-2020/src/Toys/ToyStoreGrader.java
@@ -90,7 +90,6 @@ public class ToyStoreGrader
 				System.out.println("\n");
 				
 			} catch (ReflectiveOperationException | SecurityException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
