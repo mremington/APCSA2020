a5a463c2031235c6692cdbd179bf2c99a8d54c84
mike <mike@192.168.0.16>
2020-03-19 15:51:19 -0700

    Upload unit 13

---
+++ APCS-A-2020/src/Unit13/WordSort.java
@@ -0,0 +1,32 @@
+package Unit13;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.util.Arrays;
+import static java.lang.System.*; 
+
+public class WordSort
+{
+	private String[] wordRay;
+
+	public WordSort(String line)
+	{
+	   setList(line);
+	}
+
+	public void setList(String line)
+	{
+		wordRay = line.split(" ");
+	}
+
+	public void sort()
+	{
+	}
+
+	public String toString( )
+	{
+		String output="";
+		return output+"\n\n";
+	}
+}
